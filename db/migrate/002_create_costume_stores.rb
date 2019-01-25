1dd92ea6bfa816bc481b23f439eac7f79d7de6b4
Johnathon Wood <jaywood128@gmail.com>
2019-01-25 18:12:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,9 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.text :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-
-
+      t.boolean :still_in_business 
+      t.string :opening_time 
+      t.string :closing_time 
     end
   end
 end
