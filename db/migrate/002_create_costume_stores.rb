0bd8d8262aa9751960a545e80a8b3eabc8b50d08
Johnathon Wood <jaywood128@gmail.com>
2019-01-25 18:03:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.text :location
+      t.integer :costume_inventory
+
     end
   end
 end
