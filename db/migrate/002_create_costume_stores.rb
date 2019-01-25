4705cd67164d7b459b3c1d4b613cf6ac6b199a5c
Johnathon Wood <jaywood128@gmail.com>
2019-01-25 17:17:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,9 +1,1 @@
-class CreateCostumes < ActiveRecord::Mirgration[4.2]
-
-  def change
-    create_table :costumes do |t|
-      t.text :name
-    end
-  end
-
-end# Create your costume_stores migration here
+# Create your costume_stores migration here
