c0ec6de2da2a72eb3e9a273639340b75c1daa011
Johnathon Wood <jaywood128@gmail.com>
2019-01-25 17:22:46 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,7 +3,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.text :name
-      t.integer :price
+      t.float :price
     end
   end
 end
