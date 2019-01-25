a3e5f78f554c94110825bbd633dc0c11fc5be577
Johnathon Wood <jaywood128@gmail.com>
2019-01-25 18:34:32 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,7 @@ class HauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
-      t.text :location 
+      t.text :location
     end
   end
 end# Create your haunted_houses migration here
